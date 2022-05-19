a877e95d3edfe20d0ec3d613af8ab052dfd66584
Shchurski_Eduard <shchurskied@gmail.com>
2022-05-19 17:46:42 +0300

    delete comments in index.hmtl

--- src/lesson-5/formatdate.js
+++ src/lesson-5/formatdate.js
@@ -1,4 +1,4 @@
-const str = prompt('Введите дату в формате YYYY-MM-DD','2020-11-26')
+const str = prompt('Введите дату в формате YYYY-MM-DD', '2020-11-26');
 const arr = str.split('-');
 const result = arr.reverse().join('.');
-alert(result)
+alert(result);
