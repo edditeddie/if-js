ed35e0471757e7cadadd40709bc5a1b65c75149a
Shchurski_Eduard <shchurskied@gmail.com>
2022-07-27 21:40:20 +0300

    completed lesson-10

---
+++ src/lesson-12/fetchRenderHomeGuests.js
@@ -0,0 +1,17 @@
+const parent = document.querySelector('.homes-guests__wrapper');
+const url = 'https://fe-student-api.herokuapp.com/api/hotels/popular';
+
+async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    const cards = data.map((card) => `<div class ="col-md-6 col-lg-12 col-6 homes-guests__container">
+    <div class ="homes-guests__rectangle">
+        <img class ="homes-guests__image" src=${card.imageUrl} alt=${card.name} />
+        <a class ="homes-guests__subtitle-link" href="#">${card.name}</a>
+        <div class ="homes-guests__subtitle-caption">${card.city}, ${card.country}</div>
+    </div>
+</div>`);
+    parent.insertAdjacentHTML('afterbegin', cards);
+}
+
+fetchData();
